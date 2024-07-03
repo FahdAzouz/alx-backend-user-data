8aa31aae9cf33429ef1712fe4e756c0e4ea027e4
FahdAzouz <fahdstudies00@gmail.com>
2024-07-03 23:02:29 +0100

    format

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,38 @@
 
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields, redaction, message, separator):
-    pattern = '|'.join([f"{field}=[^{separator}]*" for field in fields])
-    return re.sub(f'({pattern})', lambda m: m.group(0).split('=')[0]
-                  + '=' + redaction, message)
+    """filter the fields with pattern"""
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-16s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        redact the message of LogRecord instance
+        Args:
+        record (logging.LogRecord): LogRecord instance containing message
+        Return:
+            formatted string
+        """
+        message = super(RedactingFormatter, self).format(record)
+        redacted = filter_datum(self.fields, self.REDACTION,
+                                message, self.SEPARATOR)
+        return redacted
