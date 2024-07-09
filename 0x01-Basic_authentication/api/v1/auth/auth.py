75d206f90247bec02dedda69582f89ad54c9f16e
FahdAzouz <fahdstudies00@gmail.com>
2024-07-09 23:38:40 +0100

    task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""auhtentication module"""
+
+from flask import request
+
+
+class Auth():
+    '''class handling authentication'''
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """handles the header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns the current user"""
+        return None
