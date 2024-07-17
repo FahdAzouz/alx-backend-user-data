866ede127498196e80cc3966764a956e8f7962ad
FahdAzouz <fahdstudies00@gmail.com>
2024-07-17 20:18:51 +0100

    task 0

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''Module 0 task'''
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, String, Column
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    '''Model user database'''
+
+    def __init__(self) -> None:
+        '''initialize a new DB instance'''
+        __tablename__ = "users"
+        id = Column(Integer, primary_key=True)
+        email = Column(String(250), nullable=False)
+        hashed_password = Column(String(250), nullable=False)
+        session_id = Column(String(250), nullable=True)
+        reset_token = Column(String(250), nullable=True)
