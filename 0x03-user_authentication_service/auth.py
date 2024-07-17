0f889246e0ffd144b913c3d0caf34c943369711f
FahdAzouz <fahdstudies00@gmail.com>
2024-07-17 21:17:01 +0100

    task 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """authentification module"""
-from typing import Bytes
 from bcrypt import hashpw
 import bcrypt
 
 
-def _hash_password(password: str) -> Bytes:
+def _hash_password(password: str) -> bytes:
     '''
     returns a salted hash of the input password,
     hashed with bcrypt.hashpw.
