6ca269820f94b42de1c8d6a8ee0a2a3c01147156
FahdAzouz <fahdstudies00@gmail.com>
2024-07-17 21:10:43 +0100

    task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -61,18 +61,19 @@ class DB:
         return result
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """update user based on id"""
+        """Updates a user based on a given id.
+        """
         user = self.find_user_by(id=user_id)
         if user is None:
             return
-        update_src = {}
+        update_source = {}
         for key, value in kwargs.items():
             if hasattr(User, key):
-                update_src[getattr(User, key)] = value
+                update_source[getattr(User, key)] = value
             else:
-                raise InvalidRequestError()
+                raise ValueError()
         self._session.query(User).filter(User.id == user_id).update(
-            update_src,
+            update_source,
             synchronize_session=False,
         )
         self._session.commit()
