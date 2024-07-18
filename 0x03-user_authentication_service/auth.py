65e7ea72329b6dd0cca826073d0ff5baf07273fb
FahdAzouz <fahdstudies00@gmail.com>
2024-07-19 00:38:49 +0100

    task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,3 +88,18 @@ class Auth:
         if user_id is None:
             return None
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        '''Find the user corresponding to the email.
+        and generate a UUID and update the user’s
+        reset_token database field.
+        '''
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        token = uuid.uuid4()
+        self._db.update_user(user.id, reset_token=token)
+        return token
+
