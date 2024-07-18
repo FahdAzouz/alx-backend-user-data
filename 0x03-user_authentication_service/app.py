13b370000fa1540b0545059b62f9dd6d415a1feb
FahdAzouz <fahdstudies00@gmail.com>
2024-07-19 00:26:51 +0100

    task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,15 +49,14 @@ def login() -> str:
 
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout():
+def logout() -> str:
     '''dLogout from the session'''
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_seesion_id(session_id)
     if user is None:
         abort(403)
-    else:
-        AUTH.destroy_session(user.id)
-        return redirect("/")
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
