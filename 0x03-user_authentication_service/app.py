de7c44a315c64f81c2f1ae36038af6bd3cca6a89
FahdAzouz <fahdstudies00@gmail.com>
2024-07-19 00:25:57 +0100

    task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """App module"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -46,6 +46,18 @@ def login() -> str:
         return response
     else:
         abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    '''dLogout from the session'''
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_seesion_id(session_id)
+    if user is None:
+        abort(403)
+    else:
+        AUTH.destroy_session(user.id)
+        return redirect("/")
 
 
 if __name__ == "__main__":
